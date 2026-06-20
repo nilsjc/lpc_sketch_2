49b5a676c66e4c33044ccd932c92ef4c0e79e517
Nils Edvardsson <nils.edvardsson@sigmatechnology.com>
2026-06-20 21:36:51 +0200

    Worked with GUI and connection between core and GUI

---
+++ Core/RealtimeClass.cs
@@ -0,0 +1,94 @@
+using System;
+using PortAudioSharp;
+using Core;
+
+public class RealtimeClass
+{
+    LpcEngine _engine;
+    int _sampleRate = 44100;
+    PortAudioSharp.Stream _stream;
+    public RealtimeClass(int sampleRate = 44100)
+    {
+        _sampleRate = sampleRate;
+    }
+    public void Run(RealtimeParameters parameters)
+    {
+        _engine = new LpcEngine(_sampleRate);
+        _engine.Modulator.UseFixedPitch  = parameters.UseFixedPitch;
+        _engine.Modulator.PitchSemitones = (float)parameters.Pitch;
+        _engine.Modulator.FormantScale   = (float)parameters.Formant;
+        _engine.Modulator.FixedPitchHz   = (int)parameters.FixedPitchHz;
+        const uint framesPerBuffer = 256;   // låg latens; justera vid behov
+        
+        PortAudio.Initialize();
+
+        var inParams = new StreamParameters
+        {
+            device           = PortAudio.DefaultInputDevice,
+            channelCount     = 1,
+            sampleFormat     = SampleFormat.Float32,
+            suggestedLatency = PortAudio.GetDeviceInfo(PortAudio.DefaultInputDevice).defaultLowInputLatency,
+            hostApiSpecificStreamInfo = IntPtr.Zero
+        };
+        var outParams = new StreamParameters
+        {
+            device           = PortAudio.DefaultOutputDevice,
+            channelCount     = 1,
+            sampleFormat     = SampleFormat.Float32,
+            suggestedLatency = PortAudio.GetDeviceInfo(PortAudio.DefaultOutputDevice).defaultLowOutputLatency,
+            hostApiSpecificStreamInfo = IntPtr.Zero
+        };
+
+        PortAudioSharp.Stream.Callback callback =
+            (IntPtr input, IntPtr output, uint frameCount,
+             ref StreamCallbackTimeInfo timeInfo, StreamCallbackFlags statusFlags, IntPtr userData) =>
+        {
+            unsafe
+            {
+                var inSpan  = new ReadOnlySpan<float>((void*)input,  (int)frameCount);
+                var outSpan = new Span<float>((void*)output, (int)frameCount);
+                _engine.ProcessBlock(inSpan, outSpan);
+            }
+            return StreamCallbackResult.Continue;
+        };
+
+        _stream = new PortAudioSharp.Stream(
+            inParams, outParams, _sampleRate, framesPerBuffer,
+            StreamFlags.ClipOff, callback, IntPtr.Zero);
+
+        _stream.Start();
+    }
+    public void Stop()
+    {
+        _stream.Stop();
+        _stream.Dispose();
+        PortAudio.Terminate();
+    }
+    public void Robot(bool fixedPitch)
+    {
+        _engine.Modulator.UseFixedPitch = fixedPitch;
+    }
+    public void VoiceUnvoiced(bool voiceUnvoiced)
+    {
+        _engine.Modulator.VoiceUnvoiced = voiceUnvoiced;
+    }
+
+    public void ChangePitch(double pitch)
+    {
+        _engine.Modulator.PitchSemitones = (float)pitch;
+    }
+
+    public void ChangeFormant(double formant)
+    {
+        _engine.Modulator.FormantScale = (float)formant;
+    }
+}
+public class RealtimeParameters
+{
+    public double Pitch { get; set; }
+    public double Formant { get; set; }
+    public bool UseFixedPitch { get; set; }
+    public double FixedPitchHz { get; set; }
+    public bool UseVoicedUnvoiced { get; set; }
+
+}
--- UserGUI/ViewModels/MainWindowViewModel.cs
+++ UserGUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,123 @@
-﻿namespace UserGUI.ViewModels;
+// https://docs.avaloniaui.net/docs/data-binding/binding-to-commands
+
+using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Core;
+namespace UserGUI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public RealtimeClass Host { get; } = new RealtimeClass();
+    private bool _isHostRun { get; set; } = false;
+    private bool _fixedPitch { get; set; } = false;
+    private bool _voiceUnvoiced { get; set; } = true;
+
+    [ObservableProperty]
+    private double _pitch;
+
+    [ObservableProperty]
+    private double formant;
+
+    [ObservableProperty]
+    private double sliderValue_3;
+
+    private double _pitchSliderValue;
+    private double _formantSliderValue;
+
+    public double PitchSliderValue
+    {
+        get => _pitchSliderValue;
+        set
+        {
+            if (_pitchSliderValue != value)
+            {
+                _pitchSliderValue = value;
+                OnPropertyChanged();
+                OnPitchSliderValueChanged(value); // Call your function here
+            }
+        }
+    }
+
+    public double FormantSliderValue
+    {
+        get => _formantSliderValue;
+        set
+        {
+            if (_formantSliderValue != value)
+            {
+                _formantSliderValue = value;
+                OnPropertyChanged();
+                OnFormantSliderValueChanged(value); // Call your function here
+            }
+        }
+    }
+
+    [RelayCommand]
+    public void OnRobot()
+    {
+        if (_fixedPitch)
+        {
+            _fixedPitch = false;
+        }
+        else
+        {
+            _fixedPitch = true;
+        }
+        Host.Robot(_fixedPitch);
+    }
+
+    [RelayCommand]
+    public void OnVoiceUnvoiced()
+    {
+        if(_voiceUnvoiced)
+        {
+            _voiceUnvoiced = false;
+        }
+        else
+        {
+            _voiceUnvoiced = true;
+        }
+        Host.VoiceUnvoiced(_voiceUnvoiced);
+    }
+
+    [RelayCommand]
+    public void StartStopEngine()
+    {
+        if (_isHostRun)
+        {
+            _isHostRun = false;
+            Host.Stop();
+        }
+        else
+        {
+            _isHostRun = true;
+            Host.Run(new RealtimeParameters
+            {
+                Pitch = _pitchSliderValue,
+                Formant = _formantSliderValue,
+                UseFixedPitch = _fixedPitch,
+                UseVoicedUnvoiced = _voiceUnvoiced
+            });
+        }
+    }
+
+    public void OnPitchSliderValueChanged(double value)
+    {
+        // Implementation for slider value change
+        Host.ChangePitch(value);
+    }
+
+    public void OnFormantSliderValueChanged(double value)
+    {
+        // Implementation for formant slider value change
+        Host.ChangeFormant(value);
+    }
+
+    [RelayCommand]
+    public void OnChangeFormant()
+    {
+        // Implementation for Change Formant command
+        Host.ChangeFormant(Formant);
+    }
 }
